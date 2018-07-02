0e5675f3afdb14111b14e02a511527647b66aa66
Eduardo Fressato <eduardofressato@hotmail.com>
2018-07-02 03:40:01 -0300

    Added examples content based

--- examples/ranking_content_based.py
+++ examples/ranking_content_based.py
@@ -5,12 +5,27 @@ train = 'C:/datasets/ml-100k/train.dat'
 test = 'C:/datasets/ml-100k/test.dat'
 rank_cb = 'C:/datasets/ml-100k/rank_cb.dat'
 rank_attr = 'C:/datasets/ml-100k/rank_attr.dat'
-similarity = 'C:/datasets/ml-100k/vsm.dat'
-top_n = 10
+similarity1 = 'C:/datasets/ml-100k/sim_coo_vsm_new_version.dat'
+similarity2 = 'C:/datasets/ml-100k/sim_ldsd_iw_new_version.dat'
+similarity3 = 'C:/datasets/ml-100k/sim_vsm_new_version.dat'
+
 metrics = ('PREC', 'RECALL', 'NDCG', 'MAP')
 
-ItemAttributeKNN(train, test, similarity_file=similarity, output_file=rank_attr, rank_length=50).\
-    compute(metrics=metrics, n_ranks=[10, 20, 50])
-ContentBased(train, test, similarity_file=similarity, output_file=rank_cb, rank_length=50).\
-    compute(metrics=metrics, n_ranks=[10, 20, 50])
+print(similarity1)
+# ItemAttributeKNN(train, test, similarity_file=similarity1, output_file=rank_attr, rank_length=50).\
+#     compute(metrics=metrics, n_ranks=[10, 20])
+
+ContentBased(train, test, similarity_file=similarity1, output_file=rank_cb, rank_length=50).\
+   compute(metrics=metrics, n_ranks=[10, 20, 50])
+
+#print(similarity2)
+#ItemAttributeKNN(train, test, similarity_file=similarity2, output_file=rank_attr, rank_length=50).\
+#    compute(metrics=metrics, n_ranks=[10, 20])
+
+#ContentBased(train, test, similarity_file=similarity2, output_file=rank_cb, rank_length=50).\
+#   compute(metrics=metrics, n_ranks=[10, 20, 50])
+
+#print(similarity3)
+#ContentBased(train, test, similarity_file=similarity3, output_file=rank_cb, rank_length=50).\
+#   compute(metrics=metrics, n_ranks=[10, 20, 50])
 
--- examples/rating_prediction_mf.py
+++ examples/rating_prediction_mf.py
@@ -8,6 +8,8 @@
 
 from caserec.recommenders.rating_prediction.svdplusplus import SVDPlusPlus
 from caserec.recommenders.rating_prediction.matrixfactorization import MatrixFactorization
+from caserec.recommenders.rating_prediction.gsvdplusplus import GSVDPlusPlus
+from caserec.recommenders.rating_prediction.item_msmf import ItemMSMF
 from caserec.utils.cross_validation import CrossValidation
 
 db = 'C:/Users/forte/OneDrive/ml-100k/u.data'
@@ -28,10 +30,21 @@ te = 'C:/Users/forte/OneDrive/ml-100k/folds/0/test.dat'
 """
 
 # Cross Validation
-recommender = MatrixFactorization()
+# recommender = MatrixFactorization()
 
 # CrossValidation(input_file=db, recommender=recommender, dir_folds=folds_path, header=1, k_folds=5).compute()
 
 # # Simple
-MatrixFactorization(tr, te).compute()
-SVDPlusPlus(tr, te).compute()
+# MatrixFactorization(tr, te).compute()
+# SVDPlusPlus(tr, te).compute()
+
+train = "C:/dev/train.dat"
+test = "C:/dev/test.dat"
+metadata = "C:/dev/item_subject.dat"
+# GSVDPlusPlus(train, test, metadata_file=metadata).compute()
+similarity = "C:/dev/vsm.dat"
+
+MatrixFactorization(train, test, random_seed=0, epochs=30, stop_criteria=0.009).compute()
+ItemMSMF(train, test, similarity_file=similarity, neighbors=20,
+         random_seed=0, baseline=True, epochs=30, stop_criteria=0.009).compute()
+
